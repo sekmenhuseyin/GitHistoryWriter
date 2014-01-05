73af163949c80eb8e9e34a5ebf73e88bcd1e6eff
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2014-01-06 00:00:00 +0200

    1, 11

--- app.ps1
+++ app.ps1
@@ -1,9 +1,5 @@
 $tarih = Get-Date
 
 .\function.ps1 -dt 1/6/2014
-.\function.ps1 -dt 1/4/2015
-.\function.ps1 -dt 1/3/2016
-.\function.ps1 -dt 1/8/2017
-.\function.ps1 -dt 1/10/2018
 
 Set-Date -Date $tarih
