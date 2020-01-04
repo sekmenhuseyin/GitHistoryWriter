e36f34d435bc7c68d3c5c50f0ff3865fc08ccddc
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2020-01-05 00:00:00 +0300

    GDWcZgvAiO, 1, 19

--- app.ps1
+++ app.ps1
@@ -1,10 +1,5 @@
 $tarih = Get-Date
 
-.\function.ps1 -dt 5/1/2014
-.\function.ps1 -dt 4/1/2015
-.\function.ps1 -dt 3/1/2016
-.\function.ps1 -dt 1/1/2017
-.\function.ps1 -dt 7/1/2018
-.\function.ps1 -dt 6/1/2019
+.\function.ps1 -dt 5/1/2020
 
 Set-Date -Date $tarih
